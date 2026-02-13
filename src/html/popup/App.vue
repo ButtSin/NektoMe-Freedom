091236863f06829d5dbdf39d703607e5ffe7c6fa
Dmitrii Buttsin <dmitrii.buttsin@gmail.com>
2026-02-13 05:22:08 +0300

    Add a temporary implementation of tabs;

--- src/html/popup/App.vue
+++ src/html/popup/App.vue
@@ -1,12 +1,56 @@
-<script setup></script>
+<script setup>
+import BaseTabs from '@/components/BaseTabs.vue';
+import IconLock from '@/icons/IconLock.vue';
+import { ref } from 'vue';
 
-<template><div class="sex">dafssf</div></template>
+const tabs = [
+  { name: 'Settings', label: 'Настройки' },
+  { name: 'About', label: 'О расширении' },
+  { name: 'Help', label: 'Помочь проекту' },
+];
+
+const selectedTab = ref('Settings');
+
+const changeTab = (tabName) => {
+  selectedTab.value = tabName;
+};
+</script>
+
+<template>
+  <header class="main__header header">
+    <h1 class="header__title">
+      <span class="header__title-text">NektoMe Freedom — говорите вне лимитов</span>
+      &nbsp;
+      <span class="header__title-icon">
+        <IconLock />
+      </span>
+    </h1>
+  </header>
+  <main class="main">
+    <BaseTabs :names="tabs" :selectedTab="selectedTab" @changeTab="changeTab">
+      <div v-if="selectedTab === 'Settings'">1</div>
+      <div v-if="selectedTab === 'About'">2</div>
+      <div v-if="selectedTab === 'Help'">3</div>
+    </BaseTabs>
+  </main>
+</template>
 
 <style scoped lang="scss">
-.sex {
-  @include square(40px);
-  $i: var(--color-containers);
+.header {
+  &__title {
+    position: relative;
 
-  color: $i;
+    display: flex;
+    align-items: center;
+    width: fit-content;
+
+    &-text {
+      hyphens: auto;
+    }
+
+    &-icon {
+      display: flex;
+    }
+  }
 }
 </style>
