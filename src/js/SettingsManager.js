c91fc5dbd918b7134c1dc80a6c07906a236c6ffe
Dmitrii Buttsin <dmitrii.buttsin@gmail.com>
2026-02-18 07:12:15 +0300

    Update and fix

--- src/js/SettingsManager.js
+++ src/js/SettingsManager.js
@@ -1,14 +1,39 @@
+/* eslint-disable no-undef */
+import { ref } from 'vue';
+
 class SettingsManager {
-  keys = {
-    popup: {
+  _defaultSettings = Object.freeze({
+    theme: 'system',
+    sexFieldUnlocked: true,
+    copyUnlocked: true,
+  });
+
+  _theme = ref(this._defaultSettings.theme);
+  _sexFieldUnlocked = ref(this._defaultSettings.sexFieldUnlocked);
+  _copyUnlocked = ref(this._defaultSettings.copyUnlocked);
+
+  _keys = Object.freeze({
+    ui: {
       theme: 'theme',
     },
 
-    common: {
+    content: {
       copyUnlocked: 'copyUnlocked',
       sexFieldUnlocked: 'sexFieldUnlocked',
     },
-  };
+  });
+
+  getTheme() {
+    return this._theme;
+  }
+
+  getSexFieldUnlocked() {
+    return this._sexFieldUnlocked;
+  }
+
+  getCopyUnlocked() {
+    return this._copyUnlocked;
+  }
 
   _setSetting(storageType, key, value) {
     const storage = storageType === 'session' ? chrome.storage.session : chrome.storage.local;
@@ -36,12 +61,48 @@ class SettingsManager {
     });
   }
 
-  setLocalTheme(themeValue) {
-    return this._setSetting('local', this.keys.popup.theme, themeValue);
+  async setLocalTheme(themeValue) {
+    await this._setSetting('local', this._keys.ui.theme, themeValue);
+    this._theme.value = themeValue;
   }
 
-  getLocalTheme() {
-    return this._getSetting('local', this.keys.popup.theme);
+  async getLocalTheme() {
+    return this._getSetting('local', this._keys.ui.theme);
+  }
+
+  getDefaultSettings() {
+    return this._defaultSettings;
+  }
+
+  async loadAllSettings() {
+    const defaultSettings = this.getDefaultSettings();
+
+    const [themeRes, sexRes, copyRes] = await Promise.allSettled([
+      this.getLocalTheme(),
+      this.getLocalSexFieldUnlocked(),
+      this.getLocalCopyUnlocked(),
+    ]);
+
+    if (themeRes.status === 'fulfilled') {
+      this._theme.value = themeRes.value ?? defaultSettings.theme;
+    } else {
+      console.warn('Failed to load theme, using default:', themeRes.reason);
+      this._theme.value = defaultSettings.theme;
+    }
+
+    if (sexRes.status === 'fulfilled') {
+      this._sexFieldUnlocked.value = sexRes.value ?? defaultSettings.sexFieldUnlocked;
+    } else {
+      console.warn('Failed to load sexFieldUnlocked, using default:', sexRes.reason);
+      this._sexFieldUnlocked.value = defaultSettings.sexFieldUnlocked;
+    }
+
+    if (copyRes.status === 'fulfilled') {
+      this._copyUnlocked.value = copyRes.value ?? defaultSettings.copyUnlocked;
+    } else {
+      console.warn('Failed to load copyUnlocked, using default:', copyRes.reason);
+      this._copyUnlocked.value = defaultSettings.copyUnlocked;
+    }
   }
 
   setSessionTabState(tabStateValue, currentTab) {
@@ -53,26 +114,19 @@ class SettingsManager {
   }
 
   setLocalCopyUnlocked(copyUnlockedValue) {
-    return this._setSetting('local', this.keys.common.copyUnlocked, copyUnlockedValue);
+    return this._setSetting('local', this._keys.content.copyUnlocked, copyUnlockedValue);
   }
 
   getLocalCopyUnlocked() {
-    return this._getSetting('local', this.keys.common.copyUnlocked);
+    return this._getSetting('local', this._keys.content.copyUnlocked);
   }
 
   setLocalSexFieldUnlocked(sexFieldUnlockedValue) {
-    return this._setSetting('local', this.keys.common.sexFieldUnlocked, sexFieldUnlockedValue);
+    return this._setSetting('local', this._keys.content.sexFieldUnlocked, sexFieldUnlockedValue);
   }
 
   getLocalSexFieldUnlocked() {
-    return this._getSetting('local', this.keys.common.sexFieldUnlocked);
-  }
-
-  getDefaultSettings() {
-    return {
-      sexFieldUnlocked: true,
-      copyUnlocked: true,
-    };
+    return this._getSetting('local', this._keys.content.sexFieldUnlocked);
   }
 }
 
