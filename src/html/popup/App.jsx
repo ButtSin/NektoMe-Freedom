7a16c8078cf09e7a1c639a61e4557db1e71e3823
Dmitrii Buttsin <dmitrii.buttsin@gmail.com>
2026-05-20 05:47:50 +0300

    Update popup App;

--- src/html/popup/App.jsx
+++ src/html/popup/App.jsx
@@ -1,10 +1,40 @@
 import { useState, useEffect } from "react";
-// импорт стилей (если нужны только для этого компонента)
+import { extensionVersion } from "@/js/constants";
+import IconLock from "@/icons/IconLock.jsx";
+import MainTabs from "@/html/popup/components/MainTabs.jsx";
+import styles from "@/html/popup/App.module.scss";
 
 function App() {
-  // Здесь будет логика вашего компонента
-
-  return <div className="app">{/* JSX-разметка */}</div>;
+  return (
+    <>
+      <header className={`${styles.popup__header} ${styles.header}`}>
+        <h1 className={`${styles.header__title}`}>
+          <span className={`${styles["header__title-text"]}`}>
+            NektoMe Freedom — говорите вне лимитов
+          </span>
+          &nbsp;
+          <span
+            className={`${styles["header__title-icon"]}`}
+            aria-hidden="true"
+          >
+            <IconLock />
+          </span>
+        </h1>
+        <p className={`${styles.header__version}`}>
+          v.&nbsp;{extensionVersion}
+        </p>
+      </header>
+      <main className={`${styles.popup__main} ${styles.main}`}>
+        <MainTabs />
+      </main>
+      <footer className={`${styles.popup__footer} ${styles.footer}`}>
+        <p>
+          С уважением и признательностью посвящается моей хорошей подруге Мали.
+          Спасибо тебе за всё.
+        </p>
+      </footer>
+    </>
+  );
 }
 
 export default App;
