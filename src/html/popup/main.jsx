7bed9d02b5588841dd6ad2e601c0187ac5a2daa9
Dmitrii Buttsin <dmitrii.buttsin@gmail.com>
2026-05-20 05:48:39 +0300

    Update popup main.jsx;

--- src/html/popup/main.jsx
+++ src/html/popup/main.jsx
@@ -2,9 +2,14 @@ import { createRoot } from "react-dom/client";
 import "@/styles/main.scss";
 import App from "./App.jsx";
 import ErrorHandlers from "@/js/ErrorHandlers";
+import { StrictMode } from "react";
 
 new ErrorHandlers().promiseGlobalErrorSetup();
 
 const container = document.getElementById("app");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
