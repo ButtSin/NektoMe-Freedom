80a390784e245bda860d029b48e6a8679587a9f9
Dmitrii Buttsin <dmitrii.buttsin@gmail.com>
2026-07-19 20:52:36 +0300

    feat: sync version from manifest to package files

--- scripts/build-manifest.js
+++ scripts/build-manifest.js
@@ -1,3 +1,4 @@
+// scripts/build-manifest.js
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -7,32 +8,26 @@ const browser = process.argv[2] || "chromium";
 
 const sourceFile = path.resolve(__dirname, "../public/manifest.json");
 const destFile = path.resolve(__dirname, "../dist/manifest.json");
-const packageJsonPath = path.resolve(__dirname, "../package.json");
 
 if (!fs.existsSync(sourceFile)) {
   console.error(`❌ Файл ${sourceFile} не найден!`);
   process.exit(1);
 }
 
-const pkg = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
 const manifest = JSON.parse(fs.readFileSync(sourceFile, "utf-8"));
 
-manifest.version = pkg.version;
-
-// 4. Трансформации под Firefox
+// Трансформации под Firefox
 if (browser === "firefox") {
-  // Firefox не поддерживает service_worker, нужен scripts
   if (manifest.background?.service_worker) {
     manifest.background = {
       scripts: [manifest.background.service_worker],
     };
   }
 
-  // Firefox требует уникальный ID
   manifest.browser_specific_settings = {
     gecko: {
       id: "nektome-freedom@dmitrii.buttsin",
-      strict_min_version: "115.0", // Чтобы гарантировать поддержку MV3 и storage.session
+      strict_min_version: "115.0",
     },
   };
 }
