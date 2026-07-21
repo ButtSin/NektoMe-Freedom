e2e1548440456f850b7d394588bd5d08d17e6f24
Dmitrii Buttsin <dmitrii.buttsin@gmail.com>
2026-07-21 16:29:39 +0300

    chore: add prettier config and update eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,22 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import reactX from "@eslint-react/eslint-plugin";
-import { defineConfig, globalIgnores } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import reactX from '@eslint-react/eslint-plugin';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default defineConfig([
-  globalIgnores(["dist"]),
+  globalIgnores(['dist']),
   {
-    files: ["**/*.{js,jsx,ts,tsx}"],
+    files: ['**/*.{js,jsx,ts,tsx}'],
 
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
-      reactX.configs["recommended"],
+      reactX.configs['recommended'],
+      eslintConfigPrettier,
     ],
 
     languageOptions: {
@@ -23,13 +25,14 @@ export default defineConfig([
     },
 
     globals: {
-      chrome: "readonly",
+      chrome: 'readonly',
     },
 
     rules: {
-      "no-unused-vars": "warn",
-      "react/prop-types": "off",
-      "@eslint-react/no-missing-key": "warn",
+      'no-unused-vars': 'warn',
+      'react/prop-types': 'off',
+      '@eslint-react/no-missing-key': 'warn',
+      'no-console': 'warn',
     },
   },
 ]);
