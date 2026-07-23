24f1e5d9129304208312a87cde5e7bf4ae9532b7
Dmitrii Buttsin <dmitrii.buttsin@gmail.com>
2026-07-24 02:49:05 +0300

    chore: add extension name and version to error messages

--- src/shared/config/constants.js
+++ src/shared/config/constants.js
@@ -1,7 +1,8 @@
 const manifest = chrome.runtime.getManifest();
 
+const extensionName = manifest.extensionName;
 const extensionVersion = manifest.version;
 const isThemeChangingProvide = Symbol('isThemeChanging');
 const THEME_TRANSITION_DURATION = 300;
 
-export { extensionVersion, isThemeChangingProvide, THEME_TRANSITION_DURATION };
+export { extensionName, extensionVersion, isThemeChangingProvide, THEME_TRANSITION_DURATION };
--- src/shared/lib/ErrorHandler.js
+++ src/shared/lib/ErrorHandler.js
@@ -1,9 +1,15 @@
+import { extensionName, extensionVersion } from '../config/constants';
+
 class ErrorHandler {
   _onPromiseGlobalError(event) {
     alert(
-      'Произошла ошибка обработки асинхронного кода. Расширение может начать работать некорректно.',
+      `[${extensionName} v${extensionVersion}] Произошла ошибка обработки асинхронного кода. ` +
+        'Расширение может начать работать некорректно.',
     );
-    console.error('Nekto Me Plus. Unhandled Promise Rejection: ', event.reason);
+    console.error(
+      `[${extensionName} v${extensionVersion}] Unhandled Promise Rejection:`,
+      event.reason,
+    );
 
     event.preventDefault();
   }
