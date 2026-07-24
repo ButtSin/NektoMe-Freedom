40cdbaee6df89767fdb6b6e6de8d01d7ab5bd337
Dmitrii Buttsin <dmitrii.buttsin@gmail.com>
2026-07-24 22:00:51 +0300

    fix: use manifest.name instead of manifest.extensionName

--- src/shared/config/constants.js
+++ src/shared/config/constants.js
@@ -1,6 +1,6 @@
 const manifest = chrome.runtime.getManifest();
 
-const extensionName = manifest.extensionName;
+const extensionName = manifest.name;
 const extensionVersion = manifest.version;
 const isThemeChangingProvide = Symbol('isThemeChanging');
 const THEME_TRANSITION_DURATION = 300;
